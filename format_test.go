2c87fda17de1def6ea288141b8e7600b888e535b
David Tolnay <dtolnay@gmail.com>
2016-11-06 09:02:21 -0800

    Singular message for a single error

--- format_test.go
+++ format_test.go
@@ -5,8 +5,23 @@ import (
 	"testing"
 )
 
-func TestListFormatFunc(t *testing.T) {
-	expected := `2 error(s) occurred:
+func TestListFormatFuncSingle(t *testing.T) {
+	expected := `1 error occurred:
+
+* foo`
+
+	errors := []error{
+		errors.New("foo"),
+	}
+
+	actual := ListFormatFunc(errors)
+	if actual != expected {
+		t.Fatalf("bad: %#v", actual)
+	}
+}
+
+func TestListFormatFuncMultiple(t *testing.T) {
+	expected := `2 errors occurred:
 
 * foo
 * bar`
