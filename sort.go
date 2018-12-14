6b811fa6accb9b67501edb2c3376038a2d11bee1
Dale Hui <dhui@users.noreply.github.com>
2018-12-14 02:14:41 -0800

    Use pointers for receiver methods implementing the sort interface     - All other Error receiver methods use pointers

--- sort.go
+++ sort.go
@@ -1,16 +1,25 @@
 package multierror
 
 // Len implements sort.Interface function for length
-func (err Error) Len() int {
+func (err *Error) Len() int {
+	if err == nil {
+		return 0
+	}
 	return len(err.Errors)
 }
 
 // Swap implements sort.Interface function for swapping elements
-func (err Error) Swap(i, j int) {
+func (err *Error) Swap(i, j int) {
+	if err == nil {
+		return
+	}
 	err.Errors[i], err.Errors[j] = err.Errors[j], err.Errors[i]
 }
 
 // Less implements sort.Interface function for determining order
-func (err Error) Less(i, j int) bool {
+func (err *Error) Less(i, j int) bool {
+	if err == nil {
+		return false
+	}
 	return err.Errors[i].Error() < err.Errors[j].Error()
 }
